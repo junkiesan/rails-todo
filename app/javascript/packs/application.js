8f8556e03cbd4abd1363097d6afedc01f38f0ea0
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 17:52:22 +0200

    bug_fix: bootstrap stopped

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery-ui/ui/widget")
 require("jquery-ui/ui/widgets/sortable")
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
