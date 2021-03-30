c15895b3f8e135d40880165bca25f1bbe23d6583
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 17:44:44 +0200

    sortable: not saving the sort

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,29 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap-datepicker")
-
+require("jquery-ui/ui/widget")
+require("jquery-ui/ui/widgets/sortable")
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on("turbolinks:load", () => {
+  $("#todos").sortable({
+    handle: '.handle',
+    update: function(e, ui) {
+      Rails.ajax({
+        url: $(this).data("url"),
+        type: "PATCH",
+        data: $(this).sortable('serialize'),
+      });
+    }
+  });
+})
