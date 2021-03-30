f4e44a91e5aa8ece6cf8fe11cedbfc88af1e9544
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 15:32:08 +0200

    todos: associations and validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ApplicationRecord
+# Associations 
+  belongs_to :user
+
+# Validations
+  validates :title, presence: true
+  validates_length_of :description, minimum: 5, maximum: 60, allow_blank: true
+
 end
