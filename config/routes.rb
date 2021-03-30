7c0fcf4a80fd0453aacd7fc958951eb9fcd80c3f
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 15:42:31 +0200

    todos: routes implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # default_url_options :host => "example.com"
   root to: 'pages#home'
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :todos
 end
