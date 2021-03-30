49a8d47b4037de33c1221900160f7165df424ab0
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 17:17:03 +0200

    act as list added to model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,7 @@
 class Todo < ApplicationRecord
 # Associations 
   # belongs_to :user
-
+  acts_as_list
 # Validations
   validates :title, presence: true
   validates_length_of :description, minimum: 5, maximum: 60, allow_blank: true
