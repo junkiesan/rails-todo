b95d765023beed61d2e9774ecffa9a1b5cbfd57e
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 15:41:32 +0200

    todos: controller actions implemented

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,41 @@
 class TodosController < ApplicationController
+before_action :set_todos, only: [:show, :edit, :update, :destroy]
+def index
+  @todos = Todo.all
+end
+
+def show 
+end
+
+def new
+  @todo = Todo.new
+end
+
+def create
+  @todo = Todo.new(todo_params)
+  @todo.save
+  redirect to todos_path
+end
+
+def edit
+end
+
+def updated
+  @todo.update(todo_params)
+  redirect to todos_path
+end
+
+def destroy
+  @todo.destroy  
+  redirect to todos_path
+end
+
+private
+
+def set_todos
+  @todo = Todo.find(params[:id])
+end
+
+def todo_params
+  params.require(:todo).permit(:title, :description, :deadline)
 end
