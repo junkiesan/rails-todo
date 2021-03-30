c9eeec06ba8a34a0ac9de029177482a6ded1bfbe
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 16:11:31 +0200

    todos: status added false by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_140737) do
+ActiveRecord::Schema.define(version: 2021_03_30_140953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_140737) do
     t.datetime "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: true
   end
 
   create_table "users", force: :cascade do |t|
