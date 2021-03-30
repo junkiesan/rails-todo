352bad5b8e62af34c2acd7d6774596275cb58a6e
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 16:23:28 +0200

    bug_fix: status is now false by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_140953) do
+ActiveRecord::Schema.define(version: 2021_03_30_142215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_140953) do
     t.datetime "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "status", default: true
+    t.boolean "status", default: false
   end
 
   create_table "users", force: :cascade do |t|
