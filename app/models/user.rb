e4310ed0f2fc833639823194f9650adb28888b6d
Julien Bouland <bouland.julien@gmail.com>
2021-03-30 14:25:55 +0200

    Devise definition overrided

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  protected
+  def password_required?
+    confirmed? ? super : false
+  end
+         
 end
+        
